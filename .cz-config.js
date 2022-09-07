ad3d5aeeeea20cd3ddeed7664163f27db48942e0
lijing <jingli14@tsinghua.org.cn>
2022-09-07 15:11:29 +0800

    feat: init reposiroty

--- .cz-config.js
+++ .cz-config.js
@@ -16,7 +16,7 @@ module.exports = {
     { value: 'build', name: 'build: 打包' }
   ], // 消息步骤，正常只需要选择
   messages: {
-    type: '请选择提交类型:', // customScope: '请输入修改范围(可选):',
+    type: '请选择提交类型:', // cutomScope: '请输入修改范围(可选):',
     subject: '请简要描述提交(必填):', // body: '请输入详细描述(可选):',
     footer: '请输入要关闭的issue(可选):',
     confirmCommit: '确认使用以上信息提交？(y/n)'
